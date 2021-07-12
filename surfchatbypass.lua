af52a40282bb8212a1ca4c9cbc9910e3f5636512
dowoge <59783653+dowoge@users.noreply.github.com>
2021-07-12 02:38:04 -0400

    small characters between letters will be impossible to see/hard to

--- surfchatbypass.lua
+++ surfchatbypass.lua
@@ -14,9 +14,9 @@ h=hookfunc(call,function(...)
         if not (msg:sub(1,1)=='/'or msg:sub(1,1)=='!') then
             local final=''
             for i in msg:gmatch('.') do
-                final=final..i..'<1:'..string.char(math.random(97,122))..'>'
+                final=final..i..'<intensify:100000:<1:'..string.char(math.random(97,122))..'>>'
             end
-            return h(args[1],string.sub(final,1,#final-5))
+            return h(args[1],string.sub(final,1,#final-24))
         end
     end
     return h(...)
