0f1ab804ad820e53d96adf8278d6240ba314fae4
tommy aka doge <59783653+dowoge@users.noreply.github.com>
2022-07-22 04:34:09 -0400

    Delete autoscroll.lua

--- autoscroll.lua
+++
@@ -1,9 +0,0 @@
-local plr=game.Players.LocalPlayer
-local char=game.Workspace.Characters:WaitForChild(plr.Name)
-game:GetService('RunService').RenderStepped:Connect(function()
-    local ray=Ray.new(char.Torso.CFrame.Position,Vector3.new(0,-3.5,0))
-    local hit=workspace:FindPartOnRayWithIgnoreList(ray,{char})
-    if hit and game:GetService('UserInputService'):IsKeyDown(Enum.KeyCode.Space) then
-        mousescroll(1337)
-    end
-end)
