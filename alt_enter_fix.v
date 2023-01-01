53b17f690d8c96376363bab23ab1db1dbeeb99ba
tommy <59783653+dowoge@users.noreply.github.com>
2023-01-01 03:19:24 -0500

    syntax update + support for program files installation

--- alt_enter_fix.v
+++ alt_enter_fix.v
@@ -1,24 +1,37 @@
 import os
+
 mut rblxpath := ''
+
 if os.user_os()[0..'windows'.len] == 'windows' {
-	rblxpath = os.getenv('LOCALAPPDATA') + '\\Roblox'
-	println('User is on windows, rblxpath = $rblxpath')
-} else {
-	println('Not on windows, please enter the path to the Roblox directory')
-	rblxpath = os.input('')
+	env_pf := os.getenv('ProgramFiles(x86)')
+	env_lap := os.getenv('LOCALAPPDATA')
+
+	if os.is_dir('$env_pf\\Roblox') {
+		rblxpath = '$env_pf\\Roblox'
+	} else if os.is_dir('$env_lap\\Roblox') {
+		rblxpath = '$env_lap\\Roblox'
+	}
 }
-for folder in os.ls(rblxpath+'\\Versions')? {
+
+if rblxpath != '' {
+	println('Roblox found: $rblxpath')
+} else {
+	os.input('Roblox not found, press enter to exit')
+	exit(0)
+}
+
+for folder in os.ls(rblxpath+'\\Versions')! {
 	if folder.contains('version') {
 		versionpath := rblxpath+'\\Versions\\'+folder
 		if os.is_file(versionpath+'\\RobloxPlayerBeta.exe') {
 			if !os.is_dir(versionpath+'\\ClientSettings') {
-				os.mkdir(versionpath+'\\ClientSettings')?
+				os.mkdir(versionpath+'\\ClientSettings')!
 				println('Created ClientSettings folder in $folder')
 			} else {
 				println('ClientSettings folder already exists in $folder')
 			}
 			if !os.is_file(versionpath+'\\ClientSettings\\ClientAppSettings.json') {
-				os.write_file(versionpath+'\\ClientSettings\\ClientAppSettings.json', '{"FFlagHandleAltEnterFullscreenManually":"False"}')?
+				os.write_file(versionpath+'\\ClientSettings\\ClientAppSettings.json', '{"FFlagHandleAltEnterFullscreenManually":"False"}')!
 				println('Wrote file to $folder')
 			} else {
 				println('ClientAppSettings.json already exists in $folder')
@@ -26,4 +39,5 @@ for folder in os.ls(rblxpath+'\\Versions')? {
 		}
 	}
 }
-os.input('Done')
+
+os.input('Done, press enter to exit')
